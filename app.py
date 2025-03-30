10fbe0b1e103fe7b981769a2f9e55faf8170f57f
CoalRocks <julijangarbek@gmail.com>
2025-03-30 09:59:14 -0400

    Changed the terminal to use streamlit based UI

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from pymongo import MongoClient
+from neo4j import GraphDatabase
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import json
+
+# Add a sidebar title label
+st.sidebar.title("MongoDB")
+
+# Collect the host URI
+mongodb_host = st.sidebar.text_input("MongoDB Host URI")
+
+# Collect database name
+mongodb_database = st.sidebar.text_input("Database Name")
+
+# Collect collection name
+mongodb_collection = st.sidebar.text_input("Collection Name")
+
+# Introduce a default case
+collection = None
+
+# Launch an instance of the MongoDB databse when each requirement is fulfilled
+if mongodb_host and mongodb_database and mongodb_collection:
+    try:
+        client = MongoClient(mongodb_host) # Establish connection to client
+        db = client[mongodb_database] # Connect to client database
+        collection = db[mongodb_collection] # Connect to database collection
+        st.success(f"Connected to {mongodb_database}.{mongodb_collection}") # Return successful conncection status to database.collection
+    except Exception as e:
+        st.error(f"Failed to connect to MongoDB: {e}") # Return a connection error
+
+# Build the text input for queries into the specified MongoDB database
+st.header("MongoDB Shell")
+st.markdown("Enter MongoDB command in MQL format")
+user_input = st.text_area("MongoDB Query", height=100)
+
+# Handle the input and display the returned result
